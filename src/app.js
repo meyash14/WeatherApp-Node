5a92efccc4bfc0a535a27d1e27f2668df21cfc4f
Yashaswi Bhardwaj <meyash14@gmail.com>
2019-10-05 20:15:54 +0530

    back

--- src/app.js
+++ src/app.js
@@ -20,19 +20,19 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Andrew Mead'
-    })
-})
-
-// app.get('/about', (req, res) => {
-//     res.render('about', {
-//         title: 'About Me',
+// app.get('', (req, res) => {
+//     res.render('index', {
+//         title: 'Weather',
 //         name: 'Andrew Mead'
 //     })
 // })
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
 
 // app.get('/help', (req, res) => {
 //     res.render('help', {
