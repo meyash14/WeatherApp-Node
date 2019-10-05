789d54a07dae8b332b25d4c921ab32fd77798d0d
Yashaswi Bhardwaj <meyash14@gmail.com>
2019-10-05 19:49:49 +0530

    back

--- src/app.js
+++ src/app.js
@@ -27,12 +27,7 @@ app.get('', (req, res) => {
     })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
-    })
-})
+
 
 app.get('/help', (req, res) => {
     res.render('help', {
