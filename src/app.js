b58d4b53127fa749f97d5a53e316103937bf67c6
Yashaswi Bhardwaj <meyash14@gmail.com>
2019-10-05 19:55:40 +0530

    back

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+//app.set('view engine', 'hbs')
+//app.set('views', viewsPath)
+//hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+//app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
