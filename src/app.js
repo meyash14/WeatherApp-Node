304a17b7ca852f744f894cc1b0556922d52a05f0
Yashaswi Bhardwaj <meyash14@gmail.com>
2019-10-05 19:51:11 +0530

    back

--- src/app.js
+++ src/app.js
@@ -29,39 +29,8 @@ app.get('', (req, res) => {
 
 
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
-    })
-})
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
